d469e63851ce5ec51be15edecfdc137bba43e3f5
chayi548 <167800726+Chay548@users.noreply.github.com>
2024-11-26 21:48:18 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 48;
+
+        // Check if the number is prime
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        // Handle edge cases
+        if (num <= 1) {
+            return false; // 0 and 1 are not prime numbers
+        }
+
+        // Only check for factors from 2 to the square root of the number
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; // If divisible by any number, it's not prime
+            }
+        }
+
+        return true; // If no divisors were found, it's prime
+    }
+}
